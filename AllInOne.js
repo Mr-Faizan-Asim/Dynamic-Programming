6ceb234fba39cd938879f10726135aaf3b69d21f
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2023-12-04 15:11:44 +0500

    leet Code Task Add

--- AllInOne.js
+++ AllInOne.js
@@ -9,9 +9,15 @@ const fibWithMem = (n,memos={})=>{
     memos[n] = fibWithMem(n-1,memos) + fibWithMem(n-2,memos);
     return memos[n];
 }
+// Solution of Climb Stairs on Leet Code  Link = "https://leetcode.com/problems/climbing-stairs"
+var climbStairs = function(n,memos={}) {
+    if(n in memos) return memos[n];
+    if(n <= 1) return 1;
+    memos[n] = climbStairs(n-1,memos) + climbStairs(n-2,memos);
+    return memos[n];
+};
 
-
-console.log(fib(6));
+console.log(fibWithMem(6));
 console.log(fib(3));
 console.log(fib(4));
 //console.log(fib(50)); //The Program become very slow at it. 
