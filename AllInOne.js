c78e821448809c2d30cde97ad13cf9333ae59703
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2023-12-04 15:53:41 +0500

    Grid Traversal

--- AllInOne.js
+++ AllInOne.js
@@ -16,9 +16,19 @@ var climbStairs = function(n,memos={}) {
     memos[n] = climbStairs(n-1,memos) + climbStairs(n-2,memos);
     return memos[n];
 };
+// Grid travel from left most corner to right bottom corner m = rows and n = columns
+const  gridTravel = (m,n,memos={})=>{
+    const key = m + "," + n;
+    if(key in memos) return memos[key];
+    if(m == 0 || n == 0) return 0;
+    if(m == 1 && n == 1) return 1;
+    memos[key] =  gridTravel(m-1,n,memos)+gridTravel(m,n-1,memos);
+    return memos[key];
+}
 
 console.log(fibWithMem(6));
 console.log(fib(3));
 console.log(fib(4));
 //console.log(fib(50)); //The Program become very slow at it. 
 console.log(fibWithMem(50));
+console.log(gridTravel(50,18));
