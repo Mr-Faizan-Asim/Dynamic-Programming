4aeab9027c48a178ec5ce80cdee65ec6e0139cb8
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2023-12-04 14:35:31 +0500

    fib With Memoization

--- AllInOne.js
+++ AllInOne.js
@@ -2,8 +2,17 @@ const fib = (n) =>{
     if(n <= 2) return 1;
     return fib(n-1)+fib(n-2);
 }
+// Memoization
+const fibWithMem = (n,memos={})=>{
+    if(n in memos) return memos[n];
+    if(n <= 2) return 1;
+    memos[n] = fibWithMem(n-1,memos) + fibWithMem(n-2,memos);
+    return memos[n];
+}
+
 
 console.log(fib(6));
 console.log(fib(3));
 console.log(fib(4));
 //console.log(fib(50)); //The Program become very slow at it. 
+console.log(fibWithMem(50));
